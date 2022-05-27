1403b6415e8408a1d521c9644b8276d6a6d0b68b
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2022-05-28 02:30:30 +0300

    Add solution to maximum product

---
+++ 7-kyu/Maximum Product/index.js
@@ -0,0 +1,15 @@
+/*
+
+Task
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+*/
+
+function adjacentElementsProduct(array) {
+    let arr = []
+    
+    for (let i = 0; i < array.length - 1; i++) {
+      arr.push(array[i] * array[i + 1])
+    }
+    return Math.max(...arr)
+  }
